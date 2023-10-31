ab1a7ced86cc52dbb68d344cf2f9aa69c30ec5c1
kent108 <quentin.cutrim@gmail.com>
2023-10-31 17:30:28 +0100

    8 - Commit aprés ménage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageHomeComponent } from './pages/page-home/page-home.component';
 
 import { PageAdminComponent } from './pages/page-admin/page-admin.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { PageAddProductComponent } from './pages/page-add-product/page-add-product.component';
 import { PageConnectComponent } from './pages/page-connect/page-connect.component';
 import { ProductEditComponent } from './components/product-edit/product-edit.component';
-import { AuthGuard } from './guards/auth-guard.guard';
+
 
 const routes: Routes = [
   { path: '', redirectTo: 'admin', pathMatch: 'full' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { PageHomeComponent } from './pages/page-home/page-home.component';
+
 import { PageAdminComponent } from './pages/page-admin/page-admin.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { CardComponent } from './components/card/card.component';
+
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormProductComponent } from './components/form-product/form-product.component';
@@ -23,10 +23,10 @@ import { ProductEditComponent } from './components/product-edit/product-edit.com
   declarations: [
     AppComponent,
     NavbarComponent,
-    PageHomeComponent,
+ 
     PageAdminComponent,
     PageNotFoundComponent,
-    CardComponent,
+  
     ProductListComponent,
     FormProductComponent,
     PageAddProductComponent,
